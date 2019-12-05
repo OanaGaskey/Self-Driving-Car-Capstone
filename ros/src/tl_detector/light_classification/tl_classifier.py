6b06517d9809862c2636a5fd4b226da8cf50e838
Oana <oana.gaskey@gmail.com>
2019-12-05 14:44:12 -0500

    included model, problem loading

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,20 @@
+import os
+import cv2
+import math
+import numpy as np
+import tensorflow as tf
+from keras.models import load_model
 from styx_msgs.msg import TrafficLight
+
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        pass
+        #self.model = load_model(DIR_PATH + '/model.h5')
+        self.model = load_model('model.h5')
+        self.model._make_predict_function()
+        self.light_state = TrafficLight.UNKNOWN
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -16,4 +27,22 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        hsv_image = np.array([cv2.cvtColor(image, cv2.COLOR_BGR2HSV)])
+        
+        model_predict = self.model.predict(hsv_image)
+        predicted_state = int(model_predict.argmax(axis=-1))
+        
+        if predicted_state == 0:
+            print("RED")
+            self.light_state = TrafficLight.RED
+        elif predicted_state == 1:
+            print("YELLOW")
+            self.light_state = TrafficLight.YELLOW
+        elif predicted_state == 2:
+            print("GREEN")
+            self.light_state = TrafficLight.GREEN
+        else:
+            print("UNKNOWN")
+            self.light_state = TrafficLight.UNKNOWN
+        
+        return self.light_state
--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -127,15 +127,15 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        return light.state
-#         if(not self.has_image):
-#             self.prev_light_loc = None
-#             return False
+        #return light.state
+        if(not self.has_image):
+            self.prev_light_loc = None
+            return False
 
-#         cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
         #Get classification
-#         return self.light_classifier.get_classification(cv_image)
+        return self.light_classifier.get_classification(cv_image)
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
@@ -157,7 +157,7 @@ class TLDetector(object):
             car_wp_idx = self.get_closest_waypoint(self.pose.pose.position.x, self.pose.pose.position.y)
 
             #TODO find the closest visible traffic light (if one exists)
-            diff = len(self.waypoints.waypoints)
+            diff = 200
             for i, light in enumerate(self.lights):
                 # Get stop line waypoint index
                 line = stop_line_positions[i]
