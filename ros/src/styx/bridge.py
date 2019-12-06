1064d17c7a64c5af2be9abbc8c854ffda5af4ab1
Rajat Roy <34815254+Rajat-Roy@users.noreply.github.com>
2019-12-07 01:26:25 +0530

    Update bridge.py

--- ros/src/styx/bridge.py
+++ ros/src/styx/bridge.py
@@ -1,4 +1,3 @@
-
 import rospy
 
 import tf
@@ -44,6 +43,8 @@ class Bridge(object):
         self.yaw = None
         self.angular_vel = 0.
         self.bridge = CvBridge()
+
+        self.image_gap = 0
 
         self.callbacks = {
             '/vehicle/steering_cmd': self.callback_steering,
@@ -175,12 +176,23 @@ class Bridge(object):
         self.publishers['dbw_status'].publish(Bool(data))
 
     def publish_camera(self, data):
-        imgString = data["image"]
-        image = PIL_Image.open(BytesIO(base64.b64decode(imgString)))
-        image_array = np.asarray(image)
+        # imgString = data["image"]
+        # image = PIL_Image.open(BytesIO(base64.b64decode(imgString)))
+        # image_array = np.asarray(image)
 
-        image_message = self.bridge.cv2_to_imgmsg(image_array, encoding="rgb8")
-        self.publishers['image'].publish(image_message)
+        # image_message = self.bridge.cv2_to_imgmsg(image_array, encoding="rgb8")
+        # self.publishers['image'].publish(image_message)
+
+        if self.image_gap >= 5:
+            self.image_gap = 0
+            imgString = data["image"]
+            image = PIL_Image.open(BytesIO(base64.b64decode(imgString)))
+            image_array = np.asarray(image)
+    
+            image_message = self.bridge.cv2_to_imgmsg(image_array, encoding="rgb8")
+            self.publishers['image'].publish(image_message)
+        else:
+            self.image_gap += 1
 
     def callback_steering(self, data):
         self.server('steer', data={'steering_angle': str(data.steering_wheel_angle_cmd)})
